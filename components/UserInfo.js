f13657ddc5958acfeca9cf2392d11a6d71f78a1f
ArthurSchmidt <arthurschmidtn@gmail.com>
2025-12-19 18:51:13 -0300

    refractor: criar a classe userinfo

---
+++ components/UserInfo.js
@@ -0,0 +1,19 @@
+export default class UserInfo {
+  constructor({ nameSelector, aboutSelector }) {
+    this._nameElement = document.querySelector(nameSelector);
+    this._aboutElement = document.querySelector(aboutSelector);
+  }
+
+  getUserInfo() {
+    const userInfo = {
+      name: this._nameElement.textContent,
+      about: this._aboutElement.textContent,
+    };
+    return userInfo;
+  }
+
+  setUserInfo({ name, about }) {
+    this._nameElement.textContent = name;
+    this._aboutElement.textContent = about;
+  }
+}
